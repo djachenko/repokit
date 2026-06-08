2fa3b1b3305494eb54a79ab1a4df99036b1211ce
Igor Djachenko <i.dyachenko@okko.tv>
2026-06-09 04:16:19 +0700

    feat: skip existing template files on configure

--- init/workflows.sh
+++ init/workflows.sh
@@ -8,5 +8,10 @@ mkdir -p .github/workflows
 
 for tpl in "$SCRIPT_DIR/templates/workflows/"*.yml; do
   name=$(basename "$tpl")
-  sed "s/{{REPO}}/$REPO/g" "$tpl" > ".github/workflows/$name"
+  dest=".github/workflows/$name"
+  if [[ -f "$dest" ]]; then
+    echo "  skip $name (already exists)"
+  else
+    sed "s/{{REPO}}/$REPO/g" "$tpl" > "$dest"
+  fi
 done
