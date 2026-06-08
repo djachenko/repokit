0dad2f10d56877308ee4445a1a53e20b9362085b
Igor Djachenko <i.s.djachenko@gmail.com>
2026-06-09 04:16:10 +0700

    refactor: split repo.sh into git_init.sh and remote.sh

---
+++ init/git_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+echo "→ Initializing git..."
+git init
--- init/remote.sh
+++ init/remote.sh
@@ -4,7 +4,4 @@ set -e
 
 echo "→ Creating repository $OWNER/$REPO..."
 gh repo create "$OWNER/$REPO" --public
-
-echo "→ Initializing git..."
-git init
 git remote add origin "git@github.com:$OWNER/$REPO.git"
