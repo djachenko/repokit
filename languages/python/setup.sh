4e5a2ae020c20f34f097f55cd52839049fa7770e
Igor Djachenko <i.s.djachenko@gmail.com>
2026-06-20 02:24:48 +0700

    feat: add --force-workflows and --force-pyproject flags

--- languages/python/setup.sh
+++ languages/python/setup.sh
@@ -4,8 +4,8 @@ set -e
 
 TPL="$SCRIPT_DIR/languages/python/pyproject.toml"
 
-if [[ -f "pyproject.toml" ]]; then
-  echo "→ Writing pyproject.toml... skip (already exists)"
+if [[ -f "pyproject.toml" && "${REPOKIT_FORCE:-false}" == false ]]; then
+  echo "→ Writing pyproject.toml... skip (already exists, use --force-pyproject to overwrite)"
 else
   echo "→ Writing pyproject.toml..."
   sed "s/{{REPO}}/$REPO/g; s/{{OWNER}}/$OWNER/g" "$TPL" > pyproject.toml
