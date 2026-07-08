cd802f9d3ed1dd6135ffcc79f217c0b6cfacde49
Igor Djachenko <i.s.djachenko@gmail.com>
2026-07-08 23:21:15 +0700

    fix: suppress shellcheck warnings for dynamic source and cross-shell variables

--- languages/dotfiles/05_branch_prepare.sh
+++ languages/dotfiles/05_branch_prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shellcheck disable=SC2034  # variables are set for the parent shell via source
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BASE_BRANCH="$BRANCH"
 BRANCH_ON_REMOTE=false
