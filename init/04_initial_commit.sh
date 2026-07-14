55c11e65e92c4a1bbfbd63e45fa7b365ba20d5e5
Igor Djachenko <i.s.djachenko@gmail.com>
2026-07-14 19:59:54 +0700

    fix: explicit -b master in git init, HEAD in push

--- init/04_initial_commit.sh
+++ init/04_initial_commit.sh
@@ -11,4 +11,5 @@ git add .
 run_quiet git commit --allow-empty -m "chore: [repokit] initial setup" --author="$REPOKIT_AUTHOR"
 
 # -u sets the upstream so future `git push` / `git pull` work without arguments.
-run_quiet git push -u origin master
+# HEAD instead of a hardcoded branch name so this works regardless of init.defaultBranch.
+run_quiet git push -u origin HEAD
